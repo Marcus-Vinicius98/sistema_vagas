4aaeb59ec6147ef7599725449818ea2ce5eddfd1
Marcus Vinicius <marcusviniferrer@gmail.com>
2023-01-02 20:27:30 -0300

    composer e autoload criados

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sistema/vagas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3a33b03ed3a2fe674b3b94bd35477754f026fb58',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sistema/vagas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3a33b03ed3a2fe674b3b94bd35477754f026fb58',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
